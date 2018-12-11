52ec56117189b15729f71a530e1e56322aed824a
TrungDT <thanhtrung.it25@gmail.com>
2018-12-11 20:34:08 +0700

    initial routes

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Players from "./Players";
+import Teams from "./Teams";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+
+          <Route path="/" exact component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Teams.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Teams extends Component {
+  render() {
+    return <div className="container two-column">TEAMS</div>;
+  }
+}
