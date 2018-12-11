aa8c94e2b8b1f4e4f7c82147d73c569cbb663c7a
TrungDT <thanhtrung.it25@gmail.com>
2018-12-11 21:54:21 +0700

    players-list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getTeamNames } from "../api";
+import TeamLogo from "./TeamLogo";
 
 export default class Home extends Component {
+  state = {
+    teamNames: []
+  };
+
+  componentDidMount() {
+    getTeamNames().then(teamNames =>
+      this.setState({
+        teamNames
+      })
+    );
+  }
+
   render() {
-    return <div className="container">HOME</div>;
+    const { teamNames } = this.state;
+
+    return (
+      <div className="container">
+        <h1 className="large-header">Hash History Basketball Leage</h1>
+        <h3 className="header text-center">Select a team</h3>
+        <div className="home-grid">
+          {teamNames.map(id => (
+            <Link key={id} to={`/${id}`}>
+              <TeamLogo id={id} width="125px" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,7 +1,46 @@
 import React, { Component } from "react";
+import { getPlayers } from "../api";
+import Sidebar from "./Sidebar";
+import { parse } from "query-string";
 
 export default class Players extends Component {
+  state = {
+    players: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    const { location } = this.props;
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers();
+  }
+
+  fetchPlayers = teamId => {
+    getPlayers(teamId).then(players =>
+      this.setState({
+        loading: false,
+        players
+      })
+    );
+  };
+
   render() {
-    return <div className="container two-column">PLAYERS</div>;
+    const { loading, players } = this.state;
+    const { location } = this.props;
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Players"
+          list={players.map(player => player.name)}
+          {...this.props}
+        />
+
+        {loading === false && location.pathname === "/players" ? (
+          <div className="sidebar-instruction">Select a Player</div>
+        ) : null}
+      </div>
+    );
   }
 }
