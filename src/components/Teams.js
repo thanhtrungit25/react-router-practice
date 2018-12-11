0fd51c3b3bd9e2c8050294a1696990f12cce808c
TrungDT <thanhtrung.it25@gmail.com>
2018-12-11 22:49:01 +0700

    teams-first

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import { getTeamNames } from "../api";
 
 export default class Teams extends Component {
+  state = {
+    teamNames: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    getTeamNames().then(teamNames => {
+      this.setState({
+        teamNames,
+        loading: false
+      });
+    });
+  }
+
   render() {
-    return <div className="container two-column">TEAMS</div>;
+    const { loading, teamNames } = this.state;
+    const { location, match } = this.props;
+
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Teams"
+          list={teamNames}
+          {...this.props}
+        />
+        {loading === false && location.pathname === "/teams" ? (
+          <div className="sidebar-instruction">Select a Team</div>
+        ) : null}
+      </div>
+    );
   }
 }
