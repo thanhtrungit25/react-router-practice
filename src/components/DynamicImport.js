1032baadb6f495b6fe1c80b8e0eb43b29c8d8427
TrungDT <thanhtrung.it25@gmail.com>
2018-12-12 11:12:12 +0700

    Change project name

---
+++ src/components/DynamicImport.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class DynamicImport extends Component {
+  state = {
+    component: null
+  };
+
+  componentWillMount() {
+    this.props.load().then(mod =>
+      this.setState(() => ({
+        component: mod.default
+      }))
+    );
+  }
+
+  render() {
+    return this.props.children(this.state.component);
+  }
+}
